2fc53ea890b0866d42f6b21cb9001a49ff8568be
Anthony Tagorda <anthtago@gmail.com>
2024-08-28 13:19:22 +0800

    renamed PatientForm to ClientForm

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,4 +1,4 @@
-import PatientForm from "@/components/forms/PatientForm";
+import ClientForm from "@/components/forms/ClientForm";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -18,7 +18,7 @@ export default function Home() {
             className="mb-12 h-10 w-fit"
           />
 
-          <PatientForm />
+          <ClientForm />
 
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">© 2024 Butterfly</p>
