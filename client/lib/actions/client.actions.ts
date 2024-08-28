1c2aa2268f5546a83b01b90958681de96f5d201f
Anthony Tagorda <anthtago@gmail.com>
2024-08-28 13:21:18 +0800

    Appwrite/Database Interaction

---
+++ client/lib/actions/client.actions.ts
@@ -0,0 +1,43 @@
+"use server";
+
+import { ID, Query } from "node-appwrite";
+import { users } from "../appwrite.config";
+import { parseStringify } from "../utils";
+
+// Create User in Appwrite
+export const createUser = async (user: CreateUserParams) => {
+  try {
+    const newUser = await users.create(
+      ID.unique(),
+      user.email,
+      user.phone,
+      undefined,
+      user.name
+    );
+    console.log({ newUser });
+
+    return parseStringify(newUser);
+  } catch (error: any) {
+    if (error && error?.code === 409) {
+      const existingUser = await users.list([
+        Query.equal("email", [user.email]),
+      ]);
+
+      return existingUser?.users[0];
+    }
+  }
+};
+
+// Get User
+export const getUser = async (userId: string) => {
+  try {
+    const user = await users.get(userId);
+
+    return parseStringify(user);
+  } catch (error) {
+    console.error(
+      "An error occurred while retrieving the user details:",
+      error
+    );
+  }
+};
