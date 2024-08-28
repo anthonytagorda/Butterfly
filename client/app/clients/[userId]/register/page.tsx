391dec83fa6f5dc7460ca7eec2cea22d2234d963
Anthony Tagorda <anthtago@gmail.com>
2024-08-28 13:19:47 +0800

    Created RegisterForm

---
+++ client/app/clients/[userId]/register/page.tsx
@@ -0,0 +1,38 @@
+import RegisterForm from "@/components/forms/RegisterForm";
+import { getUser } from "@/lib/actions/client.actions";
+import Image from "next/image";
+import Link from "next/link";
+
+const Register = async ({ params: { userId }} : SearchParamProps) => {
+    const user = await getUser(userId);
+
+  return (
+    <div className="flex h-screen max-h-screen">
+
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/butterfly-logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="client"
+            className="mb-12 h-10 w-fit"
+          />
+
+          <RegisterForm user={user} />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 Butterfly
+            </p>
+            <Link href="/?admin=true" className="text-blue-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Register;
---
+++ client/components/forms/RegisterForm.tsx
@@ -0,0 +1,159 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Form, FormControl } from "@/components/ui/form";
+import { createUser } from "@/lib/actions/client.actions";
+import { UserFormValidation } from "@/lib/validation";
+
+import { CustomFormField } from "../CustomFormField";
+import SubmitButton from "../SubmitButton";
+import { FormFieldType } from "./ClientForm";
+import { RadioGroup } from "../ui/radio-group";
+import { GenderOptions } from "@/constants";
+import { RadioGroupItem } from "@radix-ui/react-radio-group";
+import { Label } from "../ui/label";
+
+const RegisterForm = ({ user }: { user: User }) => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Form handler: What the form will gather from the user?
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+  });
+
+  // Submit handler: Do something with the form values.
+  async function onSubmit({
+    name,
+    email,
+    phone,
+  }: z.infer<typeof UserFormValidation>) {
+    // ✅ This will be type-safe and validated.
+    setIsLoading(true);
+
+    try {
+      const userData = { name, email, phone };
+
+      const user = await createUser(userData);
+
+      if (user) router.push(`/clients/${user.$id}/register`);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-12 flex-1"
+      >
+        <section className="space-y-4">
+          <h1 className="header">Fill up the form</h1>
+          <p className="text-dark-700">
+            Thank you for choosing A.M. Peralta Psychological Services. Filling
+            in the required information is necessary for our Psychologist and
+            Psychotherapist to have a well-directed clinical interview with you.
+          </p>
+        </section>
+
+        <section className="space-y-6">
+          <div className="mb-9 space-y-1">
+            <h2 className="sub-header">Personal Information</h2>
+          </div>
+        </section>
+
+        {/* Full Name */}
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="name"
+          placeholder="Butterfly"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
+        />
+
+        {/* Email and Phone */}
+        <div className="flex flex-col gap-6 xl:flex-row">
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="email"
+            label="Email"
+            placeholder="butterfly@butterfly.com"
+            iconSrc="/assets/icons/email.svg"
+            iconAlt="email"
+          />
+
+          <CustomFormField
+            fieldType={FormFieldType.PHONE_INPUT}
+            control={form.control}
+            name="phone"
+            label="Phone Number"
+          />
+        </div>
+
+        {/* Date of Birth and Gender */}
+        <div className="flex flex-col gap-6 xl:flex-row">
+          <CustomFormField
+            fieldType={FormFieldType.DATE_PICKER}
+            control={form.control}
+            name="birthdate"
+            label="Date of Birth"
+          />
+
+          <CustomFormField
+            fieldType={FormFieldType.SKELETON}
+            control={form.control}
+            name="gender"
+            label="Gender"
+            renderSkeleton={(field) => (
+              <FormControl>
+                <RadioGroup
+                  className="flex h-11 gap-6 xl:justify-between"
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  {GenderOptions.map((option, i) => (
+                    <div key={option + i} className="radio-group">
+                      <RadioGroupItem value={option} id={option} />
+                      <Label htmlFor={option} className="cursor-pointer">
+                        {option}
+                      </Label>
+                    </div>
+                  ))}
+                </RadioGroup>
+              </FormControl>
+            )}
+          />
+        </div>
+
+        <div className="flex flex-col gap-6 xl:flex-row"></div>
+
+        <div className="flex flex-col gap-6 xl:flex-row"></div>
+
+        <div className="flex flex-col gap-6 xl:flex-row"></div>
+
+        <div className="flex flex-col gap-6 xl:flex-row"></div>
+
+        <div className="flex flex-col gap-6 xl:flex-row"></div>
+
+        <div className="flex flex-col gap-6 xl:flex-row"></div>
+
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
+      </form>
+    </Form>
+  );
+};
+
+export default RegisterForm;
